083c084a42f0d7c9494a1d371489cd7089286fea
Jino-Lee <90809030+Leetany@users.noreply.github.com>
2025-03-16 10:11:27 +0900

    make_Scene&Background

---
+++ Assets/Jino/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    Animator ani;
+
+    public GameObject[] bullet;
+    public Transform pos = null;
+
+
+    void Start()
+    {
+        ani = GetComponent<Animator>();    
+    }
+
+    
+    void Update()
+    {
+        float moveX = moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        float moveY = moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+
+        transform.Translate(moveX, moveY, 0);
+    }
+}
