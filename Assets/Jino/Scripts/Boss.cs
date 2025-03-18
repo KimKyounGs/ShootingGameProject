84f4ac39ce97003a0dfa98cca9f8e59c5f951586
Jino-Lee <90809030+Leetany@users.noreply.github.com>
2025-03-18 21:17:14 +0900

    불필요한 에셋 정리 및 사용할 프리팹 추가

---
+++ Assets/Jino/Scripts/Boss.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    int flag = 1;
+    int speed = 2;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (transform.position.x >= 1)
+        {
+            flag *= -1;
+        }
+        if (transform.position.x <= -1)
+        {
+            flag *= -1;
+        }
+
+        transform.Translate(flag * speed * Time.deltaTime, 0, 0);
+    }
+}
