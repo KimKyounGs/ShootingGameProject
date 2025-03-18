a8cfc54da5c0823f4886c86902b2b272a1918707
h-ooon <wlaudgns1357@gmail.com>
2025-03-18 18:09:52 +0900

    Hoon_0318.01
    사랑동이 추가 (보너스 몬스터)
        - 이동 패턴: 횡이동 오른쪽에서 왼쪽으로
        - 프리팹 후 스폰매니저
        - 충돌 스프라이트 적용
    - 플레이어 status UI 제작. exp, level 연동 완료.

---
+++ Assets/Hoon/Hoon_Scripts/SpawnManager.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject luvdisc;
+    void Start()
+    {
+        InvokeRepeating("SpawnLuvdisc", 1, 1);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void SpawnLuvdisc()
+    {
+        float randomY = Random.Range(1f, 4f);
+        Instantiate(luvdisc, new Vector3(transform.position.x,randomY,0f), Quaternion.identity);
+
+    }
+}
+
+//     public float ss = -2;
+//     public float es = 2;
+//     public float StartTime = 1; // 처음 생성 시간
+//     public float SpawnStop = 10; // 중지 시간
+//     public GameObject Monster;
+//     public GameObject Monster2;
+//     public GameObject Boss;
+
+//     bool swi = true;
+//     bool swi2 = true;
+
+//     [SerializeField]
+//     GameObject textBossWarning;
+
+//     private void Awake()
+//     {
+//         textBossWarning.SetActive(false);
+
+//         // PoolManager.Instance.CreatePool(Monster, 10);
+//     }
+//     void Start()
+//     {
+//         // InvokeRepeating("Spawn", 3, 1);
+//         StartCoroutine("RandomSpawn");
+//         Invoke("Stop", SpawnStop);
+//     }
+
+//     IEnumerator RandomSpawn()
+//     {
+//         while(swi)
+//         {
+//             yield return new WaitForSeconds(StartTime);
+//             float x = Random.Range(ss, es);
+//             Vector2 r = new Vector2(x, transform.position.y);
+//             Instantiate(Monster, r, Quaternion.identity);
+//             // GameObject enemy = PoolManager.Instance.Get(Monster);
+//             // enemy.transform.position = r;
+//         }
+//     }
+
+//     IEnumerator RandomSpawn2()
+//     {
+//         while(swi2)
+//         {
+//             yield return new WaitForSeconds(StartTime+2);
+//             float x = Random.Range(ss, es);
+//             Vector2 r = new Vector2(x, transform.position.y);
+//             Instantiate(Monster2, r, Quaternion.identity);
+//         }
+//     }
+
+//     void Stop()
+//     {
+//         swi = false;
+//         // 두번째 몬스터 코루틴 추가하면 된다~
+//         StopCoroutine("RandomSpawn");
+//         StartCoroutine("RandomSpawn2");
+//         Invoke("Stop2", SpawnStop +20);
+//     }
+
+//     void Stop2()
+//     {
+//         swi2 = false;
+//         StopCoroutine("RandomSpawn2");
+//         textBossWarning.SetActive(true);
+
+//         Vector3 pos = new Vector3(0, 3, 0);
+//         Instantiate(Boss, pos, Quaternion.identity);
+//         // StartCoroutine("RandomBossSpawn");
+//         // Invoke("Stop2", SpawnStop +20);
+//     }
+
+    
+
+//     void Update()
+//     {
+        
+//     }
+// }
---
+++ Assets/Hoon/Hoon_Scripts/bubbleBeam.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class bubbleBeam : MonoBehaviour
+{
+    public float bulletSpeed = 0.5f;
+    Animator ani;
+
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.up * bulletSpeed * Time.deltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Hoon_Enemy"))
+        {
+            collision.gameObject.GetComponent<luvdisc>().Damage(1);
+            //SoundManager.instance.SoundDie();
+            ani.SetBool("Destroy", true);
+            StartCoroutine(BubblePop());
+            //Destroy(collision.gameObject);
+        }
+    }
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);       
+    }
+
+    IEnumerator BubblePop()
+    {
+        yield return new WaitForSeconds(0.3f);
+        Destroy(gameObject);
+    }
+}
+
+
+//     public float bulletSpeed = 0.5f;
+//     public int damage = 10;
+//     public GameObject Explosion;
+//     public GameObject PowerItem;
+//     public bool ExplodeFin = false;
+//     
+
+//         else if(collision.CompareTag("Boss"))
+//         {
+//             GameObject go = Instantiate(Explosion, transform.position, Quaternion.identity);
+//             Destroy(go, 0.5f);
+//             SoundManager.instance.SoundDie();
+//         }
+//     }
+
+
+
+
+//     void Update()
+//     {
+//         transform.Translate(0, bulletSpeed * Time.deltaTime, 0);
+
+        
+//     }
+// }
