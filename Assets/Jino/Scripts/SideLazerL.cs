580799872615e3ff6aac1a0ec1d60455a8f2a294
Jino-Lee <90809030+Leetany@users.noreply.github.com>
2025-03-21 12:56:50 +0900

    그림추가

--- Assets/Jino/Scripts/SideLazerL.cs
+++ Assets/Jino/Scripts/SideLazerL.cs
@@ -6,18 +6,28 @@ public class SideLazerL : MonoBehaviour
     public GameObject Lazer1;
     public Transform pos2 = null;
     Vector2 vec;
+    bool isSidecoming = true;
+    bool isLazerFired = false;
 
+    Animator Eani;
 
     void Start()
     {
+        Eani = GetComponent<Animator>();
         Invoke("Fire_Lazer", 1.8f);
     }
 
     private void Update()
     {
-        transform.position = Vector2.Lerp(transform.position, new Vector2(-2.7f, transform.position.y), moveSpeed * Time.deltaTime);
+        if(isSidecoming)
+        {
+            SideComing();
+        }
 
-
+        if (isLazerFired)
+        {
+            MoveDown();
+        }
     }
 
     private void OnBecameInvisible()
@@ -25,15 +35,28 @@ public class SideLazerL : MonoBehaviour
         Destroy(gameObject);
     }
 
+    void SideComing()
+    {
+        transform.position = Vector2.Lerp(transform.position, new Vector2(-2.7f, transform.position.y), moveSpeed * Time.deltaTime);
+    }
+    
     void MoveDown()
     {
-        Vector2 end = new Vector2(0, -5);
-        transform.Translate(end * moveSpeed * Time.deltaTime);
+        Vector2 end = new Vector2(2, -5);
+        transform.Translate(end * 0.5f * Time.deltaTime);
     }
 
     void Fire_Lazer()
     {
         GameObject go = Instantiate(Lazer1, pos2.transform.position, Quaternion.identity);
         Destroy(go, 1.2f);
+        isSidecoming = false;
+        Invoke("StartMovingDown", 1.2f);
+    }
+
+    void StartMovingDown()
+    {
+        Eani.SetTrigger("go");
+        isLazerFired = true;
     }
 }
--- Assets/Jino/Scripts/SideLazerR.cs
+++ Assets/Jino/Scripts/SideLazerR.cs
@@ -6,16 +6,28 @@ public class SideLazerR : MonoBehaviour
     public GameObject Lazer2;
     public Transform pos2 = null;
     Vector2 vec;
+    bool isSidecoming = true;
+    bool isLazerFired = false;
 
+    Animator Eani;
 
     void Start()
     {
+        Eani = GetComponent<Animator>();
         Invoke("Fire_Lazer", 1.8f);
     }
 
     private void Update()
     {
-        transform.position = Vector2.Lerp(transform.position, new Vector2(2.7f, transform.position.y), moveSpeed * Time.deltaTime);
+        if (isSidecoming)
+        {
+            SideComing();
+        }
+
+        if (isLazerFired)
+        {
+            MoveDown();
+        }
     }
 
     private void OnBecameInvisible()
@@ -23,14 +35,28 @@ public class SideLazerR : MonoBehaviour
         Destroy(gameObject);
     }
 
+    void SideComing()
+    {
+        transform.position = Vector2.Lerp(transform.position, new Vector2(2.7f, transform.position.y), moveSpeed * Time.deltaTime);
+    }
+
     void MoveDown()
     {
-
+        Vector2 end = new Vector2(-2, -5);
+        transform.Translate(end * 0.5f * Time.deltaTime);
     }
 
     void Fire_Lazer()
     {
         GameObject go = Instantiate(Lazer2, pos2.transform.position, Quaternion.identity);
         Destroy(go, 1.2f);
+        isSidecoming = false;
+        Invoke("StartMovingDown", 1.2f);
+    }
+
+    void StartMovingDown()
+    {
+        Eani.SetTrigger("go");
+        isLazerFired = true;
     }
 }
