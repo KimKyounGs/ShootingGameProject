e7367fcf09e4230e5b07807b07b52eb34290d722
박민규 <41883207+mingyu8789@users.noreply.github.com>
2025-03-21 13:48:03 +0900

    보스 패턴 2 추가

--- Assets/mingyu/PK Script/PK Boss/PK_Boss.cs
+++ Assets/mingyu/PK Script/PK Boss/PK_Boss.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class PK_Boss : MonoBehaviour
 {
-    float flag = -2;
     float B_HP = 100;
 
     public GameObject mb;
@@ -16,38 +15,65 @@ public class PK_Boss : MonoBehaviour
     public bool boss_attack = true;
     public int attack_chose = 0;
 
+    public bool skill_1 = true;
 
-    public bool skill_1_move = false;
-    public float skill_1_speed = 50;
-    public float skill_1_turn = 0;
+
+
+    public bool skill_2 = true;
+
+
+
 
     public float cool_time = 0;
 
 
+    private void Start()
+    {
 
+    }
 
     private void Update()
     {
 
         cool_time += Time.deltaTime;
 
+
         if (boss_attack == true)
         {
-            attack_chose = Random.Range(1, 1);
-
-
-
-
-
-            if (attack_chose == 1)
+            attack_chose = Random.Range(2, 2);
+            if (attack_chose > 0)
             {
-                BossMissle();
-            }
-            else if (attack_chose > 1)
-            {
-                boss_attack = true;
+                boss_attack = false;
             }
         }
+
+
+        if (attack_chose == 1)
+        {
+            skill_1 = true;
+
+            if (attack_chose == 1 && skill_1 == true)
+            {
+                BossAttack1();
+            }
+        }
+
+
+        if (attack_chose == 2)
+        {
+            skill_2 = true;
+
+            if (attack_chose == 2 && skill_2 == true)
+            {
+                BossAttack2();
+            }
+        }
+
+        //if (attack_chose > 2)
+        //{
+        //    cool_time = 0;
+        //    boss_attack = false;
+        //}
 
 
     }
@@ -56,51 +82,91 @@ public class PK_Boss : MonoBehaviour
 
 
 
-    public void BossMissle()
+    public void BossAttack1()
     {
-        transform.Translate(flag * skill_1_speed * Time.deltaTime, 0, 0);
+        float skill_1_speed = 50;
+        float skill_1_target = -2;
+        float skill_1_turn = 0;
+
+        transform.Translate(skill_1_target * skill_1_speed * Time.deltaTime, 0, 0);
+
 
         if (cool_time > 3)
         {
+            skill_1_speed = 50f;
             cool_time = 0;
         }
 
-        if (skill_1_move == false)
+
+        if (cool_time > 0.15 && cool_time < 2)
         {
-            if (cool_time > 0.15 && cool_time < 2)
-            {
-                Instantiate(mb2, pos2.position, Quaternion.identity);
-                cool_time = 0;
-            }
+            Instantiate(mb2, pos2.position, Quaternion.identity);
+            cool_time = 0;
         }
 
 
         if (transform.position.x <= -3)
         {
             skill_1_speed = 1f;
-            flag = 3;
+            skill_1_target = 3;
         }
 
         if (transform.position.x >= 3)
         {
             skill_1_speed = 1f;
-            flag = -3;
+            skill_1_target = -3;
             skill_1_turn += 1;
         }
 
-        if (skill_1_turn == 2)
+        if (skill_1_turn == 2 && transform.position.x >= 3)
         {
-            if (transform.position.x >= 3)
-            {
                 skill_1_speed = 10f;
-                flag = -3;
-                skill_1_move = true;
-            }
+            skill_1_target = -3;
+        }
 
-            if (skill_1_move == true && transform.position.x <= 0)
-            {
-                boss_attack = false;
-            }
+        if (skill_1_turn == 2 && transform.position.x <= 0)
+        {
+            attack_chose = 0;
+            skill_1_turn = 0;
+            skill_1_speed = 50f;
+            skill_1 = false;
+            boss_attack = true;
+        }
+
+    }
+
+
+
+    public void BossAttack2()
+    {
+        float skill_2_speed = 10;
+        float skill_2_target = -2;
+        bool skill_2_move = true;
+        skill_2 = false;
+        if (skill_2_move == true)
+        {
+            transform.Translate(0, skill_2_target * skill_2_speed * Time.deltaTime, 0);
+        }
+
+        if (cool_time > 3)
+        {
+            skill_2_speed = 50f;
+            cool_time = 0;
+        }
+
+
+        if (cool_time > 0.15 && cool_time < 2)
+        {
+            Instantiate(mb2, pos2.position, Quaternion.identity);
+            cool_time = 0;
+        }
+
+
+        if (transform.position.y <= 0f)
+        {
+            Debug.LogWarning("됨");
+            skill_2_move = false;
+            skill_2_speed = 1f;
         }
 
     }
@@ -108,7 +174,18 @@ public class PK_Boss : MonoBehaviour
 
 
 
-    public void Damage(int attack)
+
+
+
+
+
+
+
+
+
+
+
+    public void Damage(int attack)  //플레이어에게 데미지를 입는 함수
     {
         B_HP -= attack;
         if (B_HP <= 0)
---
+++ Assets/mingyu/PK Script/PK Boss/PK_Boss_Bullet2.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PK_Boss_Bullet2 : MonoBehaviour
+{
+
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+}
