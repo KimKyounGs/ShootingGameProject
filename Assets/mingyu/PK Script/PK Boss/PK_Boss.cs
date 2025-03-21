b3551a35f5bf3934e7b8338bc2745501bdea4e4d
박민규 <41883207+mingyu8789@users.noreply.github.com>
2025-03-21 15:31:06 +0900

    보스패턴 추가

--- Assets/mingyu/PK Script/PK Boss/PK_Boss.cs
+++ Assets/mingyu/PK Script/PK Boss/PK_Boss.cs
@@ -16,11 +16,14 @@ public class PK_Boss : MonoBehaviour
     public int attack_chose = 0;
 
     public bool skill_1 = true;
-
+    public float skill_1_speed = 9;
+    public float skill_1_target = -2;
+    public float skill_1_turn = 0;
 
 
     public bool skill_2 = true;
-
+    public bool skill_2_move1 = true;
+    public bool skill_2_move2 = false;
 
 
 
@@ -38,9 +41,10 @@ public class PK_Boss : MonoBehaviour
         cool_time += Time.deltaTime;
 
 
-        if (boss_attack == true)
+        if (boss_attack == true && cool_time >= 3)
         {
             attack_chose = Random.Range(2, 2);
+            Debug.LogWarning(attack_chose);
             if (attack_chose > 0)
             {
                 boss_attack = false;
@@ -48,26 +52,20 @@ public class PK_Boss : MonoBehaviour
         }
 
 
-        if (attack_chose == 1)
-        {
-            skill_1 = true;
 
-            if (attack_chose == 1 && skill_1 == true)
-            {
-                BossAttack1();
-            }
+        if (attack_chose == 1 && skill_1 == true)
+        {
+            BossAttack1();
         }
 
 
-        if (attack_chose == 2)
-        {
-            skill_2 = true;
 
-            if (attack_chose == 2 && skill_2 == true)
-            {
-                BossAttack2();
-            }
+
+        if (attack_chose == 2 && skill_2 == true)
+        {
+            BossAttack2();
         }
+  
 
         //if (attack_chose > 2)
         //{
@@ -84,16 +82,12 @@ public class PK_Boss : MonoBehaviour
 
     public void BossAttack1()
     {
-        float skill_1_speed = 50;
-        float skill_1_target = -2;
-        float skill_1_turn = 0;
 
         transform.Translate(skill_1_target * skill_1_speed * Time.deltaTime, 0, 0);
 
 
         if (cool_time > 3)
         {
-            skill_1_speed = 50f;
             cool_time = 0;
         }
 
@@ -128,8 +122,8 @@ public class PK_Boss : MonoBehaviour
         {
             attack_chose = 0;
             skill_1_turn = 0;
-            skill_1_speed = 50f;
-            skill_1 = false;
+            skill_1_speed = 9f;
+            cool_time = 0;
             boss_attack = true;
         }
 
@@ -139,35 +133,99 @@ public class PK_Boss : MonoBehaviour
 
     public void BossAttack2()
     {
-        float skill_2_speed = 10;
+        float skill_2_speed = 2;
         float skill_2_target = -2;
-        bool skill_2_move = true;
-        skill_2 = false;
-        if (skill_2_move == true)
+        bool skill_2_bullet = false;
+
+        if (skill_2_move1 == true)  // 플레이어 중앙 이동후 움직임 멈추기
         {
             transform.Translate(0, skill_2_target * skill_2_speed * Time.deltaTime, 0);
+
+            if (transform.position.y <= 0f)
+            {
+                skill_2_move1 = false;
+                skill_2_bullet = true;  //공격 킴
+            }
         }
 
-        if (cool_time > 3)
+
+        if (skill_2_bullet == true)    //공격 한번만 반복되게 하기
         {
-            skill_2_speed = 50f;
+            StartCoroutine(CircleFire());
             cool_time = 0;
+            skill_2_bullet = false; //공격 끔
         }
 
 
-        if (cool_time > 0.15 && cool_time < 2)
+        if (skill_2_move2 == true)  //공격이 멈추면 플레이어 원래 위치로 이동후 움직임 멈추기
         {
-            Instantiate(mb2, pos2.position, Quaternion.identity);
-            cool_time = 0;
+            skill_2_target = 4;
+            transform.Translate(0, skill_2_target * skill_2_speed * Time.deltaTime, 0);
+
+            if (transform.position.y >= 4f) //원래 위치로 이동했다면
+            {
+                cool_time = 0;
+                skill_2_move2 = false;  //움직임 멈추기
+                skill_2_move1 = true;
+                attack_chose = 0;
+                boss_attack = true; //보스 주사위 굴리기
+            }
         }
 
 
-        if (transform.position.y <= 0f)
+
+        IEnumerator CircleFire()
         {
-            Debug.LogWarning("됨");
-            skill_2_move = false;
-            skill_2_speed = 1f;
+            bool a = true;
+
+            //공격주기
+            float attackRate = 0.01f;
+            //발사체 생성갯수
+            int count = 3;
+            //발사체 사이의 각도
+            float intervalAngle = 360 / count;
+            //가중되는 각도(항상 같은 위치로 발사하지 않도록 설정
+            float weightAngle = 0f;
+
+            //원 형태로 방사하는 발사체 생성(count 갯수 만큼)
+            while (a)
+            {
+
+                for (int i = 0; i < count; ++i)
+                {
+                    //발사체 생성
+                    GameObject clone = Instantiate(mb2, transform.position, Quaternion.identity);
+
+                    //발사체 이동 방향(각도)
+                    float angle = weightAngle + intervalAngle * i;
+                    //발사체 이동 방향(벡터)
+                    //Cos(각도)라디안 단위의 각도 표현을 위해 pi/180을 곱함
+                    float x = Mathf.Cos(angle * Mathf.Deg2Rad);
+                    //sin(각도)라디안 단위의 각도 표현을 위해 pi/180을 곱함
+                    float y = Mathf.Sin(angle * Mathf.Deg2Rad);
+
+                    //발사체 이동 방향 설정
+                    clone.GetComponent<PK_Boss_Bullet>().Move(new Vector2(x, y));
+                }
+                //발사체가 생성되는 시작 각도 설정을 위한변수
+                weightAngle += 3;
+
+                //3초마다 미사일 발사
+                yield return new WaitForSeconds(attackRate);
+
+                if (cool_time >= 10)        //10초가 지나면 발사를 멈추고 제자리에 돌아가서 공격 뭐할지 정하게 하기
+                {
+                    a = false;  //총알 발사 멈추기
+                    skill_2_move2 = true;   //제자리로 이동시키기
+                }
+            }
+
+
+
         }
+
+
+
 
     }
 
