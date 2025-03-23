2ccf45b19d91efa722ce187a7c2be8343a01762a
h-ooon <wlaudgns1357@gmail.com>
2025-03-23 20:10:39 +0900

    Hoon_0323.02
    충돌 수정

--- Assets/Hoon/Hoon_Scripts/Magikarp.cs
+++ Assets/Hoon/Hoon_Scripts/Magikarp.cs
@@ -18,7 +18,6 @@ public class Magikarp : MonoBehaviour
     public int power = 0;
     public bool isEvolved = false;
 
-<<<<<<< Updated upstream
     void Awake()
     {
         if (instance == null)
@@ -27,12 +26,11 @@ public class Magikarp : MonoBehaviour
         }
         else Destroy(gameObject);
     }
-=======
+
     public int exp = 0;
     public int level = 1;
     public Image expUI;
     public TMP_Text levelUI;
->>>>>>> Stashed changes
 
     public void Shoot()
     {
@@ -40,13 +38,8 @@ public class Magikarp : MonoBehaviour
         CanShoot = false;
 
         // if (power <= 3)
-<<<<<<< Updated upstream
             Hoon_AudioManager.instance.SFXBubbleShoot();
             Instantiate(Bullet[0], pos.transform.position, Quaternion.identity);
-
-=======
-            Instantiate(Bullet[0], pos.transform.position, Quaternion.identity);
->>>>>>> Stashed changes
         // else if (power <= 6)
         //     Instantiate(Bullet[1], pos.transform.position, Quaternion.identity);
         // else if (power <= 9)
@@ -54,36 +47,15 @@ public class Magikarp : MonoBehaviour
         // else if (power > 9)
         //     Instantiate(Bullet[3], pos.transform.position, Quaternion.identity);
     }
-<<<<<<< Updated upstream
-
-
     IEnumerator BulletCooldown()
     {
         yield return new WaitForSeconds(0.5f);
-=======
-    IEnumerator ExpGain()
-    {
-        yield return new WaitForSeconds(1);
-        expUI.fillAmount += 0.1f;
-
-        StartCoroutine(ExpGain());
-    }
-
-    IEnumerator BulletCooldown()
-    {
-        yield return new WaitForSeconds(0.25f);
->>>>>>> Stashed changes
         CanShoot = true;
     }
     void Start()
     {
         magikarpAni = GetComponent<Animator>();
-<<<<<<< Updated upstream
-
-=======
         expUI.fillAmount = 0;
-        StartCoroutine(ExpGain());
->>>>>>> Stashed changes
     }
     // private void OnTriggerEnter2D(Collider2D collision)
     // {
@@ -159,16 +131,6 @@ public class Magikarp : MonoBehaviour
             // SoundManager.instance.SoundBullet();
             Shoot();
         }
-<<<<<<< Updated upstream
-
-=======
-        if(expUI.fillAmount >= 1)
-        {
-            expUI.fillAmount = 0;
-            level ++;
-            levelUI.text = "Lv" + level.ToString();
-        }
->>>>>>> Stashed changes
                     
         //캐릭터의 월드 좌표를 뷰포트 좌표계로 변환해준다.
         Vector3 viewPos = Camera.main.WorldToViewportPoint(transform.position);
--- Assets/Hoon/Hoon_Scripts/bubbleBeam.cs
+++ Assets/Hoon/Hoon_Scripts/bubbleBeam.cs
@@ -5,10 +5,7 @@ using UnityEngine.UI;
 public class bubbleBeam : MonoBehaviour
 {
     public float bulletSpeed = 0.5f;
-<<<<<<< Updated upstream
     public GameObject bubblePop;
-=======
->>>>>>> Stashed changes
     Animator ani;
 
     void Start()
@@ -24,7 +21,6 @@ public class bubbleBeam : MonoBehaviour
     {
         if(collision.CompareTag("Hoon_Enemy"))
         {
-<<<<<<< Updated upstream
             Hoon_Monster enemy = collision.GetComponent<Hoon_Monster>();
             if (enemy != null)
             {
@@ -35,22 +31,13 @@ public class bubbleBeam : MonoBehaviour
             Destroy(gameObject);
             GameObject go = Instantiate(bubblePop, collision.transform.position, Quaternion.identity);
             Destroy(go, 0.2f);
-=======
-            collision.gameObject.GetComponent<luvdisc>().Damage(1);
-            //SoundManager.instance.SoundDie();
-            ani.SetBool("Destroy", true);
-            StartCoroutine(BubblePop());
-            //Destroy(collision.gameObject);
->>>>>>> Stashed changes
+
         }
     }
     private void OnBecameInvisible()
     {
         Destroy(gameObject);       
     }
-<<<<<<< Updated upstream
-}
-=======
 
     IEnumerator BubblePop()
     {
@@ -58,31 +45,3 @@ public class bubbleBeam : MonoBehaviour
         Destroy(gameObject);
     }
 }
-
-
-//     public float bulletSpeed = 0.5f;
-//     public int damage = 10;
-//     public GameObject Explosion;
-//     public GameObject PowerItem;
-//     public bool ExplodeFin = false;
-//     
-
-//         else if(collision.CompareTag("Boss"))
-//         {
-//             GameObject go = Instantiate(Explosion, transform.position, Quaternion.identity);
-//             Destroy(go, 0.5f);
-//             SoundManager.instance.SoundDie();
-//         }
-//     }
-
-
-
-
-//     void Update()
-//     {
-//         transform.Translate(0, bulletSpeed * Time.deltaTime, 0);
-
-        
-//     }
-// }
->>>>>>> Stashed changes
